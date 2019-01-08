4f35abedb8cd82796c231a9b1f8df9716c092fef
Michael <michael@managedkaos.com>
2019-01-08 14:12:21 -0800

    Pubsub demo complete

--- pubsub/subscribe.py
+++ pubsub/subscribe.py
@@ -8,57 +8,55 @@ import time
 from threading import Thread
 
 # Replaces with your configuration information
-redis_host = "localhost"
-redis_port = 6379
+redis_host = "redis-18008.redla.demo.redislabs.com"
+redis_port = 18008
+#redis_host = "localhost"
+#redis_port = 6379
 redis_password = ""
 
 def subscriber(name, channels):
     """Processes simulated event message from a Redis Channel"""
-    
-    # Add code to create a Redis connection
 
-  
+    # Add code to create a Redis connection
+    r = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
+
     # Add code to subscribe to the channels requested
-  
+    pubsub = r.pubsub()
+    pubsub.subscribe(*channels)
+
     done = False
     while not done:
-        
+
         # Add code to get message for Redis
-        message = None
-        
+        message = pubsub.get_message()
+
         if message is not None and message['type'] == 'message':
             print("Subscriber {} received a message: {} on channel {}".format(name, message['data'], message['channel']))
-            
+
             event = json.loads(message['data'])
             if event['type'] == 'terminate':
-                done = True  
+                done = True
 
-        time.sleep(0.001)  
-        
-    
+        time.sleep(0.001)
+
+
 def run_subscribers():
     """Sets up a pubsub simulation environment with one publisher and 5 subscribers"""
-    
-    # create listeners on 
+
+    # create listeners on
     c1 = Thread(target=subscriber, args=('A', ['user_event:view', 'process:terminate']))
     c2 = Thread(target=subscriber, args=('B', ['user_event:login', 'user_event:logout', 'process:terminate']))
     c3 = Thread(target=subscriber, args=('C', ['user_event:login', 'process:terminate']))
     c4 = Thread(target=subscriber, args=('D', ['user_event:logout', 'process:terminate']))
     c5 = Thread(target=subscriber, args=('E', ['user_event:login', 'user_event:logout', 'user_event:view', 'process:terminate']))
-    
+
     c1.start()
     c2.start()
     c3.start()
     c4.start()
     c5.start()
-            
+
 if __name__ == '__main__':
-        
-    run_subscribers()    
-        
 
-
-
-
-
+    run_subscribers()
 
